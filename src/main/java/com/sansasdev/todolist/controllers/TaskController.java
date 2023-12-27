5c3038fa348f2d32f2fa6ff9171268120825c35f
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-12-27 16:55:54 -0300

    Create TaskController and methods find

---
+++ src/main/java/com/sansasdev/todolist/controllers/TaskController.java
@@ -0,0 +1,33 @@
+package com.sansasdev.todolist.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sansasdev.todolist.entities.Task;
+import com.sansasdev.todolist.services.TaskService;
+
+@RestController
+@RequestMapping("tasks")
+public class TaskController {
+
+  @Autowired
+  TaskService taskService;
+
+  @GetMapping("/{name}")
+  public ResponseEntity<Task> findByName(String name) {
+    Task task = taskService.findByName(name);
+    return ResponseEntity.ok(task);
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<Task>> findAll(Pageable pageable) {
+    Page<Task> list = taskService.findAll(pageable);
+    return ResponseEntity.ok(list);
+  }
+
+}
