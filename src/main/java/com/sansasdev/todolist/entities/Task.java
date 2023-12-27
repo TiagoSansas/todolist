fa2c45359737852b367d47aeb26b94c44824fd97
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-12-27 15:53:59 -0300

    Created Class Task

---
+++ src/main/java/com/sansasdev/todolist/entities/Task.java
@@ -0,0 +1,49 @@
+package com.sansasdev.todolist.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_task")
+public class Task {
+
+  private UUID id;
+  private String description;
+  private boolean complete;
+
+  public Task() {
+  }
+
+  public Task(UUID id, String description, boolean complete) {
+    this.id = id;
+    this.description = description;
+    this.complete = complete;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public boolean isComplete() {
+    return complete;
+  }
+
+  public void setComplete(boolean complete) {
+    this.complete = complete;
+  }
+
+}
